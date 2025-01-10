a18f5bee1468cb13365ac76e0ffeef89b13a6d57
manoj <manoj.kpradhan89@gmail.com>
2025-01-10 22:15:42 +0530

    Updated inference code and dataset

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,7 +15,8 @@ def preprocess_data(data):
     y = data['target']
     
     # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                        random_state=42)
 
     # Standard scaling
     scaler = StandardScaler()

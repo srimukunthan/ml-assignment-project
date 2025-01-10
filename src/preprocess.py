2e77fed7f16f9a20693bbab5403a8c77f360cfef
manoj <manoj.kpradhan89@gmail.com>
2025-01-10 22:22:04 +0530

    Updated inference code and dataset

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,10 +10,9 @@ def load_data(file_path):
 
 
 def preprocess_data(data):
-    """Preprocess the data by splitting into features and target and scaling."""
-    X = data.drop(columns=['target'])  # Assuming 'target' is the column name for the labels
+    """Preprocess the data by splitting into features  target and scaling."""
+    X = data.drop(columns=['target'])
     y = data['target']
-    
     # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                         random_state=42)

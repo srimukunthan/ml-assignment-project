69c9a3d3fe5d842b5eddb3e999c56c82f3b4e1b5
Mukunthan <srimukunthan@gamil.com>
2025-01-25 11:18:58 +0530

    fixed github action issues

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,7 +9,15 @@ def test_train_and_evaluate_model():
     X_test = np.random.rand(20, 31)    # 31 features
     y_test = np.random.randint(0, 2, size=20)
 
-    model = train_model(X_train, y_train)
+    # Define parameter grid
+    param_grid = {
+        "n_estimators": [10, 50, 100, 200],
+        "max_depth": [5, 10, 15, None],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4]
+    }
+
+    model = train_model(X_train, y_train, param_grid)
     accuracy, _ = evaluate_model(model, X_test, y_test)
 
     # Check if the accuracy is within a reasonable range (between 0 and 1)
